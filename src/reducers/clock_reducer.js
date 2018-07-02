4d37094b7673cf71b0976509e17a98f26e98d7ca
vokamura <vcokamura@gmail.com>
2018-07-02 13:00:04 -0700

    Redux setup complete

---
+++ src/reducers/clock_reducer.js
@@ -0,0 +1,12 @@
+const DEFAULT_STATE = {
+    time: null
+};
+
+function clockReducer(state = DEFAULT_STATE, action) {
+    switch (action.type) {
+        default: 
+            return state;
+    }
+}
+
+export default clockReducer;
